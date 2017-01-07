e1be8bb07da1dfd538a12c52261e9f24bec4a909
vatbub <vatbub123+github@googlemail.com>
2017-01-07 23:44:54 +0100

    Trying a new tutorial

--- src/main/java/com/github/vatbub/web/controller/ApplicationController.java
+++ src/main/java/com/github/vatbub/web/controller/ApplicationController.java
@@ -2,7 +2,7 @@ package com.github.vatbub.web.controller;
 
 /*-
  * #%L
- * Hello World
+ * HelloWorld
  * %%
  * Copyright (C) 2016 - 2017 Frederik Kammel
  * %%
@@ -21,7 +21,6 @@ package com.github.vatbub.web.controller;
  */
 
 
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,32 +28,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class BaseController {
+@RequestMapping("/")
+public class ApplicationController {
 
-    private static int counter = 0;
-    private static final String VIEW_INDEX = "index";
-    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value="/Test", method = RequestMethod.GET)
     public String welcome(ModelMap model) {
+        model.addAttribute("msgArgument", "Maven Java Web Application Project: Success!");
 
-        model.addAttribute("message", "Welcome");
-        model.addAttribute("counter", ++counter);
-        logger.debug("[welcome] counter : {}", counter);
-
-        // Spring uses InternalResourceViewResolver and return back index.jsp
-        return VIEW_INDEX;
-
+        return "index";
     }
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String welcomeName(@PathVariable String name, ModelMap model) {
+    @RequestMapping(value="/Print/{arg}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String arg, ModelMap model) {
+        model.addAttribute("msgArgument", "Maven Java Web Application Project, input variable: " + arg);
 
-        model.addAttribute("message", "Welcome " + name);
-        model.addAttribute("counter", ++counter);
-        logger.debug("[welcomeName] counter : {}", counter);
-        return VIEW_INDEX;
-
+        return "index";
     }
-
 }
