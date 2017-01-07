246a60dc54cac25d133a261f63ffebc2cd213475
vatbub <vatbub123+github@googlemail.com>
2017-01-07 22:02:10 +0100

    Added initial implementation of the webapp (coming from a tutorial)

---
+++ src/main/java/com/github/vatbub/web/controller/BaseController.java
@@ -0,0 +1,60 @@
+package com.github.vatbub.web.controller;
+
+/*-
+ * #%L
+ * Hello World
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BaseController {
+
+    private static int counter = 0;
+    private static final String VIEW_INDEX = "index";
+    private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
+
+        model.addAttribute("message", "Welcome");
+        model.addAttribute("counter", ++counter);
+        logger.debug("[welcome] counter : {}", counter);
+
+        // Spring uses InternalResourceViewResolver and return back index.jsp
+        return VIEW_INDEX;
+
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String name, ModelMap model) {
+
+        model.addAttribute("message", "Welcome " + name);
+        model.addAttribute("counter", ++counter);
+        logger.debug("[welcomeName] counter : {}", counter);
+        return VIEW_INDEX;
+
+    }
+
+}
