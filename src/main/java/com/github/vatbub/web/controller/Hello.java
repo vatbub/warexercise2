c2fac11918c1d7af73569373d316d052b2365b19
vatbub <vatbub123+github@googlemail.com>
2017-01-08 01:00:43 +0100

    Removed all of that freakin spring stuff and did another tutorial and now it works!!!! :) :) :)

---
+++ src/main/java/com/github/vatbub/web/controller/Hello.java
@@ -0,0 +1,77 @@
+package com.github.vatbub.web.controller;
+
+/*-
+ * #%L
+ * HelloWorld
+ * %%
+ * Copyright (C) 2016 - 2017 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Simple Hello servlet.
+ */
+
+public final class Hello extends HttpServlet {
+
+
+    /**
+     * Respond to a GET request for the content produced by
+     * this servlet.
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are producing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<head>");
+        writer.println("<title>Sample Application Servlet Page</title>");
+        writer.println("</head>");
+        writer.println("<body bgcolor=white>");
+
+        writer.println("<table border=\"0\" cellpadding=\"10\">");
+        writer.println("<tr>");
+        writer.println("<td>");
+        writer.println("<img src=\"https://openclipart.org/download/244750/Happy-Peace-Smiley-Face.svg\">");
+        writer.println("</td>");
+        writer.println("<td>");
+        writer.println("<h1>Sample Application Servlet</h1>");
+        writer.println("</td>");
+        writer.println("</tr>");
+        writer.println("</table>");
+
+        writer.println("This is the output of a servlet that is part of");
+        writer.println("the Hello, World application.");
+
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
